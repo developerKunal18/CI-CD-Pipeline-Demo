db6f405cbc0ac05c55805013887477ec0d72c59a
kunal karbhari <fullstackdeveloperkunal18@gmail.com>
2026-06-30 22:20:32 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "CI/CD Pipeline Demo"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
